319374a41715d689c42bc8e3da6b4f4721369108
aozhiqiang <1194522057@qq.com>
2020-03-12 21:35:56 +0800

    登录页静态页面，form表单，非法字符过滤，全局邮箱，密码验证工具方法

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import store from "./store";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+//import './styles/main.styl'
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
